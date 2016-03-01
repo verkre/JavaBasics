29a5b09f8218b2651c4e94c6234d62305ea37203
Vera Kreuter <vera.kreuter@gmail.com>
2016-03-01 09:20:29 +0100

    EP Action checks whether problem-objects already exist before instantiating them

--- src/courseprojectprimes/actions/ActionEulerProblems.java
+++ src/courseprojectprimes/actions/ActionEulerProblems.java
@@ -1,23 +1,42 @@
 package courseprojectprimes.actions;
 
-import courseprojectprimes.actions.eulerproblems.Ep2;
-import courseprojectprimes.actions.eulerproblems.Ep4;
-import courseprojectprimes.actions.eulerproblems.Ep7;
-import courseprojectprimes.actions.eulerproblems.Ep75;
+import courseprojectprimes.actions.eulerproblems.*;
 import courseprojectprimes.*;
 import java.util.ArrayList;
 
 public class ActionEulerProblems extends Action {
-
+    
+    Ep2 ep2Object;
+    Ep3 ep3Object;
+    Ep4 ep4Object;
+    Ep7 ep7Object;
+    Ep75 ep75Object;
+    
     public ActionEulerProblems() {
+        if (ep2Object == null) {
+            ep2Object = new Ep2();
+        }
+        if (ep3Object == null) {
+            ep3Object = new Ep3();
+        }
+        if (ep4Object == null) {
+            ep4Object = new Ep4();
+        }
+        if (ep7Object == null) {
+            ep7Object = new Ep7();
+        }
+        if (ep75Object == null) {
+            ep75Object = new Ep75();
+        }
     }
 
     // TODO these objects live as long as we stay in this action object - once we return to the main menu they are gone
     // and so are their cached solutions (better: keep objects and their stored solution as long as the program runs)
-    static Ep2 ep2Object = new Ep2();
-    static Ep4 ep4Object = new Ep4();
-    static Ep7 ep7Object = new Ep7();
-    static Ep75 ep75Object = new Ep75();
+    
+//    ep3Object = new Ep3();
+//    ep4Object = new Ep4();
+//    ep7Object = new Ep7();
+//    ep75Object = new Ep75();
     
     @Override
     public String giveDescription() {
@@ -37,6 +56,7 @@ public class ActionEulerProblems extends Action {
         ArrayList<Action> availableEulerProblems = new ArrayList<>();
         availableEulerProblems.add(new ExitToMainMenu());
         availableEulerProblems.add(ep2Object);
+        availableEulerProblems.add(ep3Object);
         availableEulerProblems.add(ep4Object);
         availableEulerProblems.add(ep7Object);
         availableEulerProblems.add(ep75Object);
