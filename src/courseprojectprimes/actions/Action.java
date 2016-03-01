89bd5c9d6a30bb9119520b07e412387cee3c9e50
Vera Kreuter <vera.kreuter@gmail.com>
2016-03-01 08:56:33 +0100

    comment

--- src/courseprojectprimes/actions/Action.java
+++ src/courseprojectprimes/actions/Action.java
@@ -17,8 +17,4 @@ public abstract class Action implements Executable {
         // getDescription to Executable interface, or make it abstract here?
     }
     
-    
-    // since this class now implements the interface Executable (with method executable()) this abstract method is not necessary anymore
-//    @Override
-//    public abstract void execute();
 }
